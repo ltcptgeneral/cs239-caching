44dd3592b0b191f523c4ce2f7bfac3891095594d
Xuanzhe Han <hanxuanzhe2002@gmail.com>
2025-03-10 23:24:00 +0000

    Revert "fix tiered results"
    This reverts commit dbaf99af9e22b25fb0ab61552e696e5f9c8cb25a.

--- tests/weighted_friend_readonly.py
+++ tests/weighted_friend_readonly.py
@@ -9,6 +9,8 @@ from utils import print_report
 
 baseurl = "http://localhost:8000"
 
+chance_of_selecting_friend = 1
+
 user_friends = json.loads(requests.get(baseurl + "/users_and_friends").content)
 user_ids = json.loads(requests.get(baseurl + "/users").content)["ids"]
 
@@ -25,25 +27,23 @@ def generate_random_friend(user):
     next_user = str(random.choice(user_friends[user]))
     return next_user
 
-for chance_of_selecting_friend in [0.25, 0.5, 0.75, 1.0]:
+times = []
+hits = []
 
-    times = []
-    hits = []
+start = time.time()
+curr_user = generate_random()
+last_user = curr_user
+for i in tqdm(range(10000)):
+    url = baseurl + "/user/" + curr_user
+    response = requests.get(url)
+    content = json.loads(response.content)
+    times.append(content["time_ms"])
+    hits.append(content["source"] == "cache")
+    if fetch_friend(chance_of_selecting_friend):
+        curr_user = generate_random_friend(last_user)
+    else:
+        curr_user = generate_random()
+        last_user = curr_user
+end = time.time()
 
-    start = time.time()
-    curr_user = generate_random()
-    last_user = curr_user
-    for i in tqdm(range(10000)):
-        url = baseurl + "/user/" + curr_user
-        response = requests.get(url)
-        content = json.loads(response.content)
-        times.append(content["time_ms"])
-        hits.append(content["source"] == "cache")
-        if fetch_friend(chance_of_selecting_friend):
-            curr_user = generate_random_friend(last_user)
-        else:
-            curr_user = generate_random()
-            last_user = curr_user
-    end = time.time()
-
-    print_report(hits, times, end - start)
+print_report(hits, times, end - start)
